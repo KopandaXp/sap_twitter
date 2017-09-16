d0957416b24ffc30992685e643b995fbcec8f980
kopanda <kopanda0612@gmail.com>
2017-09-16 02:54:29 +0000

    1

--- webapp/Component.js
+++ webapp/Component.js
@@ -22,7 +22,7 @@ function(
 		init: function () {
 			// Отключаем кэш, чтобы не было ошибки, связанной с отсутствием файла Component-changes.json
 			sap.ui.fl.Cache.setActive(false);
-
+			this.applyPatch();
 			// Объявляем одата модели
 			var oComponent = this;
 			var oResourceBundle = oComponent.getModel("i18n").getResourceBundle();
@@ -36,18 +36,29 @@ function(
 			
 			UIComponent.prototype.init.apply(this, arguments);
 			oComponent.getRouter().initialize();
+		},
+		applyPatch: function () {
+			String.prototype.to_rfc3986 = function () {
+				var tmp = encodeURIComponent( this );
+				tmp = tmp.replace( /!/g,'%21' );
+				tmp = tmp.replace( /\*/g,'%2A' );
+				tmp = tmp.replace( /\(/g,'%28' );
+				tmp = tmp.replace( /\)/g,'%29' );
+				tmp = tmp.replace( /\'/g,'%27' );
+				return tmp;
+			};
 		}
 	});
 });
 
-window.convertCMYKtoRGB = function (sCMYK) {
-	// принимает строку вида 11 22 33 44
-	var aCMYK = sCMYK.split(" ");
-	var c = aCMYK[0], m = aCMYK[1], y = aCMYK[2], k = aCMYK[3];
-	c /= 100; m /= 100; y /= 100; k /= 100;
-	var r, g, b;
-	r = Math.min(255, Math.ceil(255 * (1 - c) * (1 - k)));
-	g = Math.min(255, Math.ceil(255 * (1 - m) * (1 - k)));
-	b = Math.min(255, Math.ceil(255 * (1 - y) * (1 - k)));
-	return "rgb(" + r + ", " + g + ", " + b + ");";
-}
+// window.convertCMYKtoRGB = function (sCMYK) {
+// 	// принимает строку вида 11 22 33 44
+// 	var aCMYK = sCMYK.split(" ");
+// 	var c = aCMYK[0], m = aCMYK[1], y = aCMYK[2], k = aCMYK[3];
+// 	c /= 100; m /= 100; y /= 100; k /= 100;
+// 	var r, g, b;
+// 	r = Math.min(255, Math.ceil(255 * (1 - c) * (1 - k)));
+// 	g = Math.min(255, Math.ceil(255 * (1 - m) * (1 - k)));
+// 	b = Math.min(255, Math.ceil(255 * (1 - y) * (1 - k)));
+// 	return "rgb(" + r + ", " + g + ", " + b + ");";
+// }
--- webapp/view/MyPage.controller.js
+++ webapp/view/MyPage.controller.js
@@ -23,14 +23,71 @@ sap.ui.define([
 			
 		},
 		onPress: function (oEvent) {
+			var oAPIData = {
+				status: "Hello World"
+			};
+			var sMethod = "POST",
+				sBaseURL = window.location.origin + "/twitter/1.1/statuses/update.json",
+				sURLParameters = "?include_entities=true",
+				sURL = sBaseURL + sURLParameters,
+				sStatus = "Hello world",
+				sIncludeEntities = "true",
+				sOAuthConsumerKey = "",
+				sOAuthNonce = "",
+				sOAuthSignatureMethod = "",
+				sOAuthTimestamp = "",
+				sOAuthToken = "",
+				sOAuthVersion = "1.0";
+			var oOAuthKeys = {
+				status: sStatus,
+				include_entities: sIncludeEntities,
+				oauth_consumer_key: sOAuthConsumerKey,
+				oauth_nonce: sOAuthNonce,
+				oauth_signature_method: sOAuthSignatureMethod,
+				oauth_timestamp: sOAuthTimestamp,
+				oauth_token: sOAuthToken,
+				oauth_version: sOAuthVersion
+			};
+			var aOAuthKeys = [];
+			for (var i in oOAuthKeys) {
+				aOAuthKeys.push(i.to_rfc3986() + "=" + oOAuthKeys[i].to_rfc3986());
+			}
+			aOAuthKeys.sort(function (a, b) {return a > b;});
+			var sOAuthKeys = aOAuthKeys.join("&");
+			var sOAuthSignature = 
+				sMethod + "&" +
+				sBaseURL.to_rfc3986() + "&" +
+				sOAuthKeys.to_rfc3986();
+			
+			console.log("aOAuthKeys", aOAuthKeys);
 			$.ajax({
-				url: "http://api.twitter.com",
-				success: function () {
-					console.log("success", arguments);
-				},
-				error: function () {
-					console.log("error", arguments);
-				}
+				method: sMethod,
+				url: sURL,
+				async: false,
+				 beforeSend: function(req) {
+				 	req.setRequestHeader("Accept", "*/*");
+				 	req.setRequestHeader("Connection", "close");
+				 	req.setRequestHeader("User-Agent", "OAuth gem v0.4.4");
+				 	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				 	req.setRequestHeader("Content-Length", "76");
+				 	req.setRequestHeader("Host", "api.twitter.com");
+				 	req.setRequestHeader("status", "Hello world");
+				 	req.setRequestHeader("Authorization", 
+					 	'OAuth ' +
+						'oauth_consumer_key="mjpp4cUeysMM0ruMjjPCjJMZc",' +
+						'oauth_nonce="' + btoa(Math.random()) + '",' +
+						'oauth_signature="' + sOAuthSignature + '",' +
+						'oauth_signature_method="HMAC-SHA1",' +
+						'oauth_timestamp="1318622958",' +
+						'oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",' +
+						'oauth_version="1.0"'
+					);
+				 }/*,
+				data: oAPIData*/
+			}).done(function () {
+				console.log("success", arguments);
+			}).fail(function () {
+				console.log("error", arguments);
 			});
 		}
 	});
